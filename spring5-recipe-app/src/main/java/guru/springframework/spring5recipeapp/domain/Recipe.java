593afc38eeb69f02a5bb9164651336cb2ff13a68
GiskardB <giskard80@gmail.com>
2018-03-08 19:13:54 +0100

    jpa testing

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -24,8 +24,32 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
