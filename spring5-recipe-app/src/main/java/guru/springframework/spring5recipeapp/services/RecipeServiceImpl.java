41b95958c7accc2f713bb2bdfadf5c59b9c0fbf6
GiskardB <giskard80@gmail.com>
2018-03-12 11:18:06 +0100

    jpa testing

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -3,12 +3,14 @@ package guru.springframework.spring5recipeapp.services;
 import guru.springframework.spring5recipeapp.RecipeService;
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+
+        log.debug("find all recipes");
         Iterable<Recipe> recipes = recipeRepository.findAll();
 
         Set<Recipe> retRecipes = new HashSet<>();
